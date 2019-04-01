6e3f91339007f997b4ab39782783fa8a6a999163
garvey <15316218227@163.com>
2019-04-01 22:39:23 +0800

    政策公告页初始化展示

---
+++ src/main/java/com/garvey/property/dao/PublicityInfoPropMapper.java
@@ -0,0 +1,19 @@
+package com.garvey.property.dao;
+
+import com.garvey.property.model.PublicityInfoProp;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PublicityInfoPropMapper {
+    int deleteByPrimaryKey(Long idx);
+
+    int insert(PublicityInfoProp record);
+
+    int insertSelective(PublicityInfoProp record);
+
+    PublicityInfoProp selectByPrimaryKey(Long idx);
+
+    int updateByPrimaryKeySelective(PublicityInfoProp record);
+
+    int updateByPrimaryKey(PublicityInfoProp record);
+}
---
+++ src/main/java/com/garvey/property/model/PublicityInfoProp.java
@@ -0,0 +1,23 @@
+package com.garvey.property.model;
+
+public class PublicityInfoProp{
+    private Long idx;
+
+    private Byte visible;
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public Byte getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Byte visible) {
+        this.visible = visible;
+    }
+}
